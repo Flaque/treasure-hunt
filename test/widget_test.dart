ce5d5d54476b54205a49b8ffbb9daa3bad28be54
Flaque <econrad@zagmail.gonzaga.edu>
2018-07-04 19:20:35 -0700

    Remove boilerplate

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:treasure/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new Treasure());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
