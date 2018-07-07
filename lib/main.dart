b5331e261e6d70b252f0f0315ab2e5d33ae1053b
Flaque <econrad@zagmail.gonzaga.edu>
2018-07-07 10:49:08 -0700

    Add base frame

--- lib/main.dart
+++ lib/main.dart
@@ -31,12 +31,98 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Center(
+      body: new Container(
+        margin: EdgeInsets.only(top: 80.0),
         child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[new TextField()],
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            expandRow(new Item(
+                text: "Dogpatch in the scene bruhh",
+                distance: 2.0,
+                postedAt: new DateTime.now())),
+            expandRow(new Item(
+                text: "Hey", distance: 2.0, postedAt: new DateTime.now())),
+            expandRow(new Item(
+                text: "Hey", distance: 2.0, postedAt: new DateTime.now()))
+          ],
         ),
       ),
     );
+  }
+}
+
+Row expandRow(Widget widget) => new Row(
+      children: <Widget>[
+        Expanded(child: widget),
+      ],
+    );
+
+class ExpandedRowItem extends StatelessWidget {
+  final String text;
+
+  const ExpandedRowItem({Key key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(child: new Item(text: text)),
+      ],
+    );
+  }
+}
+
+class Item extends StatelessWidget {
+  final String text;
+  final double distance;
+  final DateTime postedAt;
+
+  const Item({Key key, this.text, this.distance, this.postedAt})
+      : super(key: key);
+
+  static const double spacing = 10.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        decoration: new BoxDecoration(
+            border:
+                new Border(top: BorderSide(color: Colors.black, width: 20.0))),
+        margin: const EdgeInsets.fromLTRB(
+            spacing * 4, spacing, spacing * 4, spacing),
+        padding: const EdgeInsets.only(top: spacing),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Row(
+              children: <Widget>[
+                new Expanded(child: Label(distanceStr())),
+                new Label(postedAtStr()),
+              ],
+            ),
+            new Container(
+                margin: const EdgeInsets.only(top: spacing, bottom: spacing),
+                child: Text(
+                  text,
+                  textAlign: TextAlign.start,
+                ))
+          ],
+        ));
+  }
+
+  String distanceStr() => distance.toString() + " miles away";
+
+  String postedAtStr() => daysAgo().toString() + " days ago";
+
+  int daysAgo() => (DateTime.now().difference(postedAt)).inDays;
+}
+
+class Label extends StatelessWidget {
+  final String text;
+  const Label(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text(text);
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TreasureTheme {
+  static ThemeData data = new ThemeData(
+      primaryColor: const Color(0xff0097E6),
+      primaryColorDark: const Color(0xff2F3640),
+      backgroundColor: Colors.white,
+      canvasColor: Colors.white,
+      brightness: Brightness.light);
+}
